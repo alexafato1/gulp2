848826449cae9a7a5dcefe7f72e04dad87bdf027
Alexa <oleksandramalakutsko@gmail.com>
2021-03-20 22:40:56 +0200

    base html + css

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,56 @@
 const {src, dest, parallel, series, watch} = require('gulp');
+const sass = require('gulp-sass');
+const notify = require('gulp-notify');
+const rename = require('gulp-rename');
+const cleanCSS = require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+const fileinclude = require('gulp-file-include');
+
+//soucemap, rename, autoprefix, cleanCSS, brows
+const styles = () => {
+  return src('./src/scss/**/*.scss')
+         .pipe(sourcemaps.init())
+         .pipe(sass({
+             outputStyle: 'expanded'
+         }).on('error', notify.onError()))
+         .pipe(rename({
+             suffix: '.min'
+            }))
+         .pipe(autoprefixer({
+             cascade: false,
+         }))
+         .pipe(cleanCSS({
+             level: 2
+         }))
+         .pipe(sourcemaps.write('.'))
+         .pipe(dest('./app/css/'))
+         .pipe(browserSync.stream())
+}
+
+const htmlInclude = () =>{
+    return src(['./src/index.html'])
+            .pipe(fileinclude({
+                prefix: '@@',
+                basepath: '@file'
+            }))
+            .pipe(dest('./app'))
+            .pipe(browserSync.stream())
+}
+
+const watchFiles = () => {
+    browserSync.init({
+        server: {
+            baseDir: "./app"
+        }
+    });
+    watch('./src/scss/**/*.scss', styles)
+    watch('./src/index.html', htmlInclude)
+}
+
+exports.styles = styles;
+exports.watchFiles = watchFiles;
+exports.fileinclude = htmlInclude;
+
+exports.default = series(htmlInclude, styles, watchFiles)
